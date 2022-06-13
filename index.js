c10409cfd0c23ee57db21e8145e7df87f9450e00
Ahmed Sharif <ahmedsharif7605276@gmail.com>
2022-06-13 22:56:16 +0600

    services on appointment route

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
@@ -10,6 +11,35 @@ app.use(express.json())
 app.get('/', (req, res) => {
     res.send('Hello Doctors Portal');
 })
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.i1jek.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const serviceCollection = client.db('doctors').collection('services')
+
+        // show services on appointment route
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+
 
 app.listen(port, () => {
     console.log(`Portal Is Working ${port}`);
